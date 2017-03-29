bef100b86366ccb4e8587ea62020ac09c0d28875
Wilfrido Nuqui <dofreewill22@gmail.com>
2017-03-29 23:49:56 +0800

    Bump version to 0.8.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
+## 0.8.0
+
+Update README to only add as a module dependency.
+ - Presence is not an OTP application. It is just a module library with functions.
+
 ## 0.7.0
 
 Initial release
--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Presence.Mixfile do
     [
       app: :presence,
       app: :excoveralls,
-      version: "0.7.0",
+      version: "0.8.0",
       elixir: "~> 1.4",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
