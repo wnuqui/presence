525e2276b2c920b988e2d2088b1212bd4aeb2717
Wilfrido Nuqui <dofreewill22@gmail.com>
2017-04-05 19:58:47 +0800

    Bump version to 0.8.5

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Presence.Mixfile do
     [
       app: :presence,
       app: :excoveralls,
-      version: "0.8.2",
+      version: "0.8.5",
       elixir: "~> 1.4",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
