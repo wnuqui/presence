fc943fa84142acc130aa4811f6ba8fbad85eb6b8
Matthew Chadwick <matthew.chadwick@gmail.com>
2021-08-20 15:13:59 -0500

    Fix "warning: found quoted keyword "coveralls" but the quotes are not required."

--- mix.exs
+++ mix.exs
@@ -16,7 +16,7 @@ defmodule Presence.Mixfile do
       # Coveralls
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [
-        "coveralls": :test,
+        coveralls: :test,
         "coveralls.detail": :test,
         "coveralls.post": :test,
         "coveralls.html": :test
